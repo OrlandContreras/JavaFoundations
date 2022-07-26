4b37a72a7960ffb55fc8e19cb9ef7f1053c087fa
Orland Contreras <orland102@gmail.com>
2022-07-25 22:09:09 -0500

    Regex Expressions: CaptureGroups - Add comments in regular expressions

--- src/main/java/com/learning/javafoundation/regularexpressions/CaptureGroups.java
+++ src/main/java/com/learning/javafoundation/regularexpressions/CaptureGroups.java
@@ -5,18 +5,24 @@ import java.util.regex.Pattern;
 
 public class CaptureGroups {
     public static void main(String[] args) {
-        String regex = "(?<countryCode>\\d{1,2}[-.,\\s]?)?(?<areaCode>\\d{3}[-.,\\s]?)(?<exchange>\\d{3}[-.,\\s]?)(?<lineNumber>\\d{4,})";
-        String phoneNumber = "57 321-333-7652";
-        // System.out.println(phoneNumber.matches(regex));
-        Pattern phoneNumberPat = Pattern.compile(regex);
+        String regex = """
+                    # This is regex to parse the parts of a phone number
+                    (?:(?<countryCode>\\d{1,2})[-.,\\s]?)? # Get's country code
+                    (?<areaCode>\\d{3})[-.,\\s]? # Get's area code
+                    (?<exchange>\\d{3})[-.,\\s]? # Get's exchange
+                    (?<lineNumber>\\d{4,}) # Get's line number
+                    """;
+        String phoneNumber = "57 321.333-7652";
+
+        Pattern phoneNumberPat = Pattern.compile(regex, Pattern.COMMENTS);  // Comments in regular expressions
         Matcher phoneNumberMat = phoneNumberPat.matcher(phoneNumber);
 
         if (phoneNumberMat.matches()) {
-            System.out.println(phoneNumberMat.group("countryCode"));
-            System.out.println(phoneNumberMat.group("areaCode"));
-            System.out.println(phoneNumberMat.group("exchange"));
-            System.out.println(phoneNumberMat.group("lineNumber"));
-            System.out.println(phoneNumberMat.group(0));
+            System.out.println("Country code: ".concat(phoneNumberMat.group("countryCode")));
+            System.out.println("Area code: ".concat(phoneNumberMat.group("areaCode")));
+            System.out.println("Exchange: ".concat(phoneNumberMat.group("exchange")));
+            System.out.println("Line number: ".concat(phoneNumberMat.group("lineNumber")));
+            System.out.println("Complete number: ".concat(phoneNumberMat.group(0)));
 
         }
     }
