2e391abeaccb9d54cb66e07350e671b3e41abecb
exiaghost <orland102@gmail.com>
2022-07-30 20:01:36 -0500

    Parsing real text document. Create TranscriptParser.java and add resource file Transcript_Example.txt

---
+++ src/main/java/com/learning/javafoundation/regularexpressions/TranscriptParser.java
@@ -0,0 +1,14 @@
+package com.learning.javafoundation.regularexpressions;
+
+public class TranscriptParser {
+    public static void main(String[] args) {
+        String transcript = """
+                Student Number:	1234598872			Grade:		11
+                Birthdate:		01/02/2000			Gender:	M
+                State ID:		8923827123
+                                
+                Cumulative GPA (Weighted)		3.82
+                Cumulative GPA (Unweighted)	3.46
+                """;
+    }
+}
