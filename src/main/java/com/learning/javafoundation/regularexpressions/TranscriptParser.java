fb5ed9964e9556a786efaa1d2ce1ecb49f6e5f0a
exiaghost <orland102@gmail.com>
2022-07-30 22:13:06 -0500

    Parsing real text document. Create regular expression.

--- src/main/java/com/learning/javafoundation/regularexpressions/TranscriptParser.java
+++ src/main/java/com/learning/javafoundation/regularexpressions/TranscriptParser.java
@@ -1,5 +1,8 @@
 package com.learning.javafoundation.regularexpressions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TranscriptParser {
     public static void main(String[] args) {
         String transcript = """
@@ -10,5 +13,28 @@ public class TranscriptParser {
                 Cumulative GPA (Weighted)		3.82
                 Cumulative GPA (Unweighted)	3.46
                 """;
+        String regex = """
+                    Student\\sNumber:\\s(?<studentNum>\\d{10}).* # Grab student number
+                    Grade:\\s+(?<grade>\\d{1,2}).* # Grab the grade
+                    Birthdate:\\s+(?<birthMonth>\\d{2})/(?<birthDay>\\d{2})/(?<birthYear>\\d{4}).* # Grab birthdate
+                    Gender:\\s(?<gender>\\w+)\\b.* # Grab the gender
+                    State\\sID:\\s+(?<stateID>\\d+).* # Grab the state ID
+                    Weighted\\)\\s+(?<weightedGPA>[\\d.]+).* # Grab the Weighted
+                    Unweighted\\)\\s+(?<unweightedGPA>[\\d.]+).* # Grab the Unweighted
+                    """;
+        Pattern pat = Pattern.compile(regex,Pattern.DOTALL | Pattern.COMMENTS);
+        Matcher mat = pat.matcher(transcript);
+        if (mat.matches()) {
+            System.out.println(mat.group("studentNum"));
+            System.out.println(mat.group("grade"));
+            System.out.println(mat.group("birthMonth"));
+            System.out.println(mat.group("birthDay"));
+            System.out.println(mat.group("birthYear"));
+            System.out.println(mat.group("gender"));
+            System.out.println(mat.group("stateID"));
+            System.out.println(mat.group("weightedGPA"));
+            System.out.println(mat.group("unweightedGPA"));
+        }
+        
     }
 }
