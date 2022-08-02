8c334271886bcd0098595c3e54b232a0084e12cf
Orland Contreras <orland102@gmail.com>
2022-08-01 20:44:30 -0500

    Numbers: Create NumberStuff class

---
+++ src/main/java/com/learning/javafoundations/numbers/NumberStuff.java
@@ -0,0 +1,24 @@
+package com.learning.javafoundations.numbers;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class NumberStuff {
+    public static void main(String[] args) {
+        BigDecimal num1 = new BigDecimal(".03828435");
+        BigDecimal num2 = new BigDecimal("5.321");
+
+        MathContext mc = new MathContext(5, RoundingMode.HALF_UP);
+
+        System.out.println(num1.divide(num2, mc));
+        System.out.println(num1.add(num2));
+        System.out.println(num1.subtract(num2));
+        System.out.println(num1.multiply(num2));
+        System.out.println(num1.sqrt(mc));
+        System.out.println(num1.max(num2));
+        System.out.println(num1.min(num2));
+        System.out.println(num1.abs(mc));
+        System.out.println(num2.remainder(new BigDecimal("2")));
+    }
+}
