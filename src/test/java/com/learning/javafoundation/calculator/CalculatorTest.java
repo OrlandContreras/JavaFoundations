78fcdbb11ced04139950abff081dc67b98a4b136
Orland Contreras <orland102@gmail.com>
2022-08-05 23:16:51 -0500

    Writing test

--- src/test/java/com/learning/javafoundation/calculator/CalculatorTest.java
+++ src/test/java/com/learning/javafoundation/calculator/CalculatorTest.java
@@ -1,15 +1,35 @@
 package com.learning.javafoundation.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
     @Test
     public void canAddZeroPlusZero() {
-        Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
         assertEquals(0, sum, "Was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
+    }
+
+    @Test
+    public void canAddMaxIntPlusOne() {
+        int sum = calc.add(Integer.MAX_VALUE, 1);
+        System.out.println(sum); // 2147483647
+        assertEquals(Integer.MAX_VALUE + 1L, sum);
     }
 }
