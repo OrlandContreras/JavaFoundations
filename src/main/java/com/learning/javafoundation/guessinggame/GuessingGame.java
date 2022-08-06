f4f5b0b3db90aa7fa705f3253c3da7e866fbddcd
Orland Contreras <orland102@gmail.com>
2022-08-06 17:09:55 -0500

    Implement TDD - GuessingGame

--- src/main/java/com/learning/javafoundation/guessinggame/GuessingGame.java
+++ src/main/java/com/learning/javafoundation/guessinggame/GuessingGame.java
@@ -1,11 +1,15 @@
 package com.learning.javafoundation.guessinggame;
 
+import java.util.Random;
+
 public class GuessingGame {
+    private final int randomNumber = new Random().nextInt(10) +1;
+
     public String guess(int guessNumber) {
         return guessNumber ==  getRandomNumber() ? "You got it" : "You didn't get it";
     }
 
     public int getRandomNumber() {
-        return 0;
+        return randomNumber;
     }
 }
--- src/test/java/com/learning/javafoundation/guessinggame/GuessingGameTest.java
+++ src/test/java/com/learning/javafoundation/guessinggame/GuessingGameTest.java
@@ -1,6 +1,7 @@
 package com.learning.javafoundation.guessinggame;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,28 +17,31 @@ public class GuessingGameTest {
 
     @Test
     public void testSimpleWinSituation() {
-        int randomNum = game.getRandomNumber();
-        String message = game.guess(randomNum);
+        int randomNum = this.game.getRandomNumber();
+        String message = this.game.guess(randomNum);
         assertEquals("You got it", message);
     }
 
     @Test
     public void testOneWrongNegGuessSituation() {
-        String message = game.guess(-5);
+        String message = this.game.guess(-5);
         assertEquals("You didn't get it", message);
     }
 
     @Test
     public void testOneWrongPosGuessSituation() {
-        int randomNum = game.getRandomNumber();
-        String message = game.guess(randomNum + 1);
+        int randomNum = this.game.getRandomNumber();
+        String message = this.game.guess(randomNum + 1);
         assertEquals("You didn't get it", message);
     }
 
+
+    //@RepeatedTest(10)
     @Test
     public void testRandomNumberGeneration() {
         int[] rndNumCount = new int[11];
-        for (int counter = 0; counter < 100; counter++) {
+        for (int counter = 0; counter < 50; counter++) {
+            GuessingGame game = new GuessingGame();
             int randomNum = game.getRandomNumber();
             rndNumCount[randomNum] = 1;
         }
